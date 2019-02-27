de2295e94225a9a090f21a2ce13ca08909a1ecb8
gopigoppu <gopi.learner93@gmail.com>
2019-02-27 23:49:41 +0530

    api service test specs add

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { of, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,15 @@ export class ApiService {
   readonly searchUrl = 'https://api.github.com/search/repositories?q=';
   readonly userFetchUrl = 'https://api.github.com/users/';
 
-  constructor(private http: HttpClient) { }
+  constructor(public http: HttpClient) { }
 
   getRepos(query) {
     return this.http.get(this.searchUrl + query);
   }
   getUserDetail(userid) {
     return this.http.get(this.userFetchUrl + userid);
+  }
+  getTeam(team: any): Observable<any> {
+    return of(team);
   }
 }
